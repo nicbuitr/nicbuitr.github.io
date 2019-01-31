0ab0be9232e43cb53f6fcf712ee7f3024908b789
nicbuitr <n.buitrago29@uniandes.edu.co>
2019-01-31 15:35:40 -0500

    Updated documentation for pushing to 2 remotes with 1 push

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <!-- This index is only used to be able to deploy tu heroku, copy it to docs folder after build -->
-<!-- Then: cd docs && git init && heroku git:remote -a nicbuitr && git pull heroku master && git add -A && git commit -m \"Commit\" && git push heroku master -->
+<!-- Then: cd docs && git init && git remote add origin https://github.com/nicbuitr/nicbuitr.github.io.git && git remote set-url --add --push origin https://github.com/nicbuitr/nicbuitr.github.io.git && git remote set-url --add --push origin https://git.heroku.com/nicbuitr.git && git pull heroku master && git add -A && git commit -m \"Commit\" && git push heroku master -->
 <?php header( 'Location: /index.html' ) ;  ?>

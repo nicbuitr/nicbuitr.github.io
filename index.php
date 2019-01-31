ee2a54df5766ffc6f91e2362e48ecde84d0d4025
nicbuitr <n.buitrago29@uniandes.edu.co>
2019-01-31 12:44:17 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- This index is only used to be able to deploy tu heroku, copy it to docs folder after build -->
+<!-- Then: cd docs && git init && heroku git:remote -a nicbuitr && git pull heroku master && git add -A && git commit -m \"Commit\" && git push heroku master -->
+<?php header( 'Location: /index.html' ) ;  ?>
